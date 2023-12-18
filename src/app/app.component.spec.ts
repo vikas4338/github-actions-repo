53c840c24edcfd649233cb11bacb8a2a4adb2189
vikas4338 <vikas4338@gmail.com>
2023-12-18 16:58:53 -0600

    moved into folder structure

--- src/app/app.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  it('should render title', () => {
-    expect(true).toBe(true);
-  });
-});
